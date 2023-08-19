9e955de65e19a2fe63b2719646b0981de03a0369
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-20 00:32:21 +0530

    redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 //import './index.css'
 import {Provider} from 'react-redux';
-
+import {store} from './services/store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
     <App />
     </Provider>
   </React.StrictMode>,
--- src/services/store.js
+++ src/services/store.js
@@ -7,4 +7,4 @@ export const store=configureStore({
     },
     middleware:(getDefaultMiddleware)=>
     getDefaultMiddleware().concat(articleApi.middleware)
-})
+});
