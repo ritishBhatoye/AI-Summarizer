a90e2a7b631e1b8fb6ccf56c956360a62047dd3d
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-20 15:45:20 +0530

    commit

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -8,19 +8,31 @@ const Demo = () => {
     url:'',
     summary:'',
   });
-const [allArticle,setAllArticle]=useState([]);
+const [allArticle,setAllArticles]=useState([]);
 const [getSummary,{error,isFetching}]=useLazyGetSummaryQuery();
+
+useEffect=(()=>{
+const articlesFromLocalStorage=JSON.parse(
+  localStorage.getItem('articles')
+)
+if(articlesFromLocalStorage){
+  setAllArticles(articlesFromLocalStorage);
+}
+},[]);
 
 const handleSubmit=async(e)=>{
   e.preventDefault();
-
   const {data} =await getSummary({articleUrl:article.url});
   if(data?.summary){
     const newArticle={...article,summary:data.summary}
-
+    const updatedAllArticles=[newArticle,...allArticle];
+     
     setArticle(newArticle);
+    setAllArticles(updatedAllArticles);
 
-    console.log(newArticle);
+    localStorage.setItem('articles',JSON.stringfy(
+      updatedAllArticles
+    ))
   }
 }
 
@@ -58,7 +70,24 @@ peer-focus:text-gray-700'
   </button>
    </form>
    {/* Browser URL History */}
-      
+      <div className='flex flex-col gap-1 max-h-60
+      overflow-y-auto'>
+      {allArticle.map((item,index)=>(
+        <div
+        key={`link-${index}`}
+        onClick={()=>setArticle(item)}
+        className='link_card'
+        >
+        <div className='copy_btn'>
+        <img 
+        src={copy}
+        alt="copy_icon"
+        className='w-[40%] h-[40%] object-contain'
+        />
+          </div>           
+          </div>
+      ))}
+      </div>
        </div>
 
       {/* Display Results */}
